f48fee2fd25509ec2e7d1372530db12e317f5a85
AmanRajVerma10 <verma.aman464@gmail.com>
2024-09-05 14:56:36 +0530

    the movie database integration

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -6,5 +6,6 @@ export const envVars={
     MONGO_URI: process.env.MONGO_URI,
     PORT: process.env.PORT || 5000,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    TMDB_API_KEY: process.env.TMDB_API_KEY
 }
---
+++ backend/services/tmdb.service.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { envVars } from "../config/envVars.js";
+
+export const fetchFromTMDB = async (url) => {
+  const options = {
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + envVars.TMDB_API_KEY,
+    },
+  };
+  const response = await axios.get(url, options);
+  if (response.status !== 200) {
+    throw new Error("Failed to fetch data", response.statusText);
+  }
+  return response.data;
+};

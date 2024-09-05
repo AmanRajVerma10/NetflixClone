48471b6adea5be270feccf3bb8c5b079ea94b02b
AmanRajVerma10 <verma.aman464@gmail.com>
2024-09-05 15:56:39 +0530

    Movie controllers and routes sorted

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -1,11 +1,12 @@
 import { fetchFromTMDB } from "../services/tmdb.service.js";
 
-export const getTrendingMovie = async (req,res) => {
+export const getTrendingMovie = async (req, res) => {
   try {
     const data = await fetchFromTMDB(
       "https://api.themoviedb.org/3/trending/movie/day?language=en-US"
     );
-    const randomMovie = data.results[Math.floor(Math.random() * data.results?.length)];
+    const randomMovie =
+      data.results[Math.floor(Math.random() * data.results?.length)];
     return res.status(200).json({ status: true, content: randomMovie });
   } catch (error) {
     console.log("Error in controller", error.message);
@@ -15,15 +16,67 @@ export const getTrendingMovie = async (req,res) => {
   }
 };
 
-export const getMovieTrailers=async(req,res)=>{
-    const {id}=req.params;
-    try {
-        const data= await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
-        return res.status(200).json({success:true,trailers:data.results})
-    } catch (error) {
-        if(error.message.includes('404')){
-            return res.status(404).send(null);
-        }
-        return res.status(500).json({success:false,message:'Internal Server Error'})
+export const getMovieTrailers = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+    );
+    return res.status(200).json({ success: true, trailers: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
     }
-}
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getMovieDetails = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+    return res.status(200).json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+    console.log("Error in getMovieDetails Controller", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getSimilarMovies = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`
+    );
+    return res.status(200).json({ success: true, similar: data.results });
+  } catch (error) {
+    console.log("Error in getSimilarMovies Controller", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getMoviesByCategory = async (req, res) => {
+    const { category } = req.params;
+    try {
+      const data = await fetchFromTMDB(
+        `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`
+      );
+      return res.status(200).json({ success: true, content: data.results });
+    } catch (error) {
+      console.log("Error in getMoviesByCategory Controller", error.message);
+      return res
+        .status(500)
+        .json({ success: false, message: "Internal Server Error" });
+    }
+  };
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,9 +1,18 @@
-import express from 'express';
-import { getTrendingMovie,getMovieTrailers } from '../controllers/movie.controller.js';
+import express from "express";
+import {
+  getTrendingMovie,
+  getMovieTrailers,
+  getMovieDetails,
+  getSimilarMovies,
+  getMoviesByCategory,
+} from "../controllers/movie.controller.js";
 
-const router= express.Router();
+const router = express.Router();
 
-router.get('/trending',getTrendingMovie);
-router.get('/:id/trailers',getMovieTrailers);
+router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
+router.get("/:category", getMoviesByCategory);
 
 export default router;

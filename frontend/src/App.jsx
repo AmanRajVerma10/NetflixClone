04c367f0b52a6801d4b97a80c7b269f11fa64aa9
AmanRajVerma10 <verma.aman464@gmail.com>
2024-09-14 15:02:19 +0530

    Search page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/AuthUser";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -39,6 +40,10 @@ function App() {
         <Route
           path="/watch/:id"
           element={user ? <WatchPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/search"
+          element={user ? <SearchPage /> : <Navigate to={"/login"} />}
         />
       </Routes>
       <Footer />
